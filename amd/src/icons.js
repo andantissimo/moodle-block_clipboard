cc144338a784a7a98879c8240535e91270681081
MALU <contact@andantissimo.jp>
2016-04-05 04:22:34 +0900

    in progress

--- amd/src/icons.js
+++ amd/src/icons.js
@@ -1,17 +1,22 @@
-define([ 'jquery', 'core/ajax', 'core/templates', 'core/notification' ], function($, ajax, templates, notification) {
+define([ 'jquery', 'jqueryui', 'core/ajax', 'core/templates', 'core/notification' ], function($, _, ajax, tpl, popup) {
     function call(methodname, args, done) {
-        ajax.call([ { methodname: methodname, args: args } ])[0].done(done).fail(notification.exception);
+        ajax.call([ { methodname: methodname, args: args } ])[0].done(done).fail(popup.exception);
     }
     return {
         init: function() {
             var $content = $('.block_favorites .content');
-            function refresh() {
-                call('block_favorites_content', {}, function(context) {
-                    templates.render('block_favorites/content', context).done(function(html) {
-                        $content.html(html);
-                    }).fail(notification.exception);
-                });
-            }
+            $content.find('.type_activity').draggable({ revert: true });
+            $('div.sitetopic, li.section').droppable({
+                accept: '.type_activity',
+                hoverClass: 'highlight',
+                drop: function(event, ui) {
+                    var $target = $(event.target);
+                    var courseid = Number($('body').attr('class').match(/course-(\d+)/)[1]);
+                    var section = $target.hasClass('sitetopic') ? 0 : Number($target.attr('id').match(/(\d+)$/)[1]);
+                    var cmid = Number(ui.draggable.attr('class').match(/starred-(\d+)/)[1]);
+                    window.console.log('courseid = ' + courseid + ', section = ' + section + ', cmid = ' + cmid);
+                }
+            });
             $('.section li.activity').each(function() {
                 var $activity = $(this), cmid = Number($activity.attr('id').match(/(\d+)$/)[1]);
                 var $icon = $('<div class="block_favorites-icon"/>');
@@ -26,7 +31,11 @@ define([ 'jquery', 'core/ajax', 'core/templates', 'core/notification' ], functio
                         } else {
                             $icon.removeClass('starred');
                         }
-                        refresh();
+                        call('block_favorites_content', {}, function(content) {
+                            tpl.render('block_favorites/content', content).done(function(html) {
+                                $content.html(html).find('.type_activity').draggable({ revert: true });
+                            }).fail(popup.exception);
+                        });
                     });
                 });
                 $activity.prepend($icon);
