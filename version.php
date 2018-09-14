71fbf058c68cd8b57b8a75d74bf5905e88ecd884
MALU <contact@andantissimo.jp>
2018-09-14 09:06:33 +0900

    comment

--- version.php
+++ version.php
@@ -8,5 +8,5 @@ defined('MOODLE_INTERNAL') || die;
 
 $plugin->component = 'block_favorites';
 $plugin->version   = 2017090900;
-$plugin->requires  = 2016052300;
+$plugin->requires  = 2016052300; // Moodle 3.1
 $plugin->maturity  = MATURITY_RC;
