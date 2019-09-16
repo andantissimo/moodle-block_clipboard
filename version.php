1fc710a7a56f38ff68bc88150087857435e44688
MALU <contact@andantissimo.jp>
2019-09-17 02:59:32 +0900

    stable version

--- version.php
+++ version.php
@@ -9,4 +9,4 @@ defined('MOODLE_INTERNAL') || die;
 $plugin->component = 'block_clipboard';
 $plugin->version   = 2019091500;
 $plugin->requires  = 2018051700; // Moodle 3.5
-$plugin->maturity  = MATURITY_RC;
+$plugin->maturity  = MATURITY_STABLE;
